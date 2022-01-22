694857c4e522ab422eda6375a250ac301262cedb
Thales Ruano <thalesoruano@gmail.com>
2022-01-22 19:46:07 -0300

    Implemented POST /sign-up and GET /tweets routes

---
+++ server.js
@@ -0,0 +1,94 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.options('*', cors());
+app.use(cors());
+
+app.post('/sign-up', (req, res)=> {
+	const { username, avatar } = req.body;
+    const user = {
+        username,
+        avatar
+    };
+    database.users.push(user);
+    res.send("Ok");
+});
+
+app.get('/tweets', (req, res)=> {
+	const lastTweets = database.tweets.slice(-10);
+    res.send(lastTweets.reverse());
+});
+
+const database = {
+    users: [],
+    tweets: [
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 2"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 3"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 4"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 5"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 6"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 7"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 8"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 9"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 10"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 11"
+        },
+        {
+            username: "bobesponja",
+            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
+            tweet: "eu amo o hub 12"
+        },
+    ]
+}
+
+app.listen('5000', () => {
+	console.log('Server is ready to rock 😆');
+});
