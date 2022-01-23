e12721d34095d70caaafea5c3b930e8a1e0ff7c9
Thales Ruano <thalesoruano@gmail.com>
2022-01-23 19:01:40 -0300

    Data validation implemented

--- server.js
+++ server.js
@@ -10,93 +10,104 @@ app.use(cors());
 
 app.post('/sign-up', (req, res)=> {
 	const { username, avatar } = req.body;
-    const user = {
-        username,
-        avatar
-    };
-    database.users.push(user);
-    res.send("Ok");
+    if(username && avatar)
+    {
+        if(isValidImage(avatar))
+        {
+            const user = {
+                username,
+                avatar
+            };
+            database.users.push(user);
+            res.send("Ok");
+        }
+        else
+        {
+            res.status(400);
+            res.send("O endereço da imagem não é válido!");   
+        }
+    }
+    else
+    {
+        res.status(400);
+        res.send("Todos os campos são obrigatórios!");
+    }
 });
 
 app.post('/tweets', (req, res)=> {
 	const { username, tweet } = req.body;
-    const tweetBody = {
-        username,
-        tweet
-    };
-    database.tweets.push(tweetBody);
-    res.send("Ok");
+    if(username && tweet)
+    {
+        const tweetBody = {
+            username,
+            tweet
+        }
+        database.tweets.push(tweetBody);
+        res.send("Ok");
+    }
+    else
+    {
+        res.status(400);
+        res.send("Todos os campos são obrigatórios!");
+    }
 });
 
 app.get('/tweets', (req, res)=> {
-	const lastTweets = database.tweets.slice(-10);
-    res.send(lastTweets.reverse());
+	const lastTweets = database.tweets.slice(-10).reverse();
+    const tweetsToReturn = [];
+    lastTweets.forEach(t => {
+        const avatar = database.users.find(u=> u.username === t.username).avatar;
+        tweetsToReturn.push({
+            ...t,
+            avatar: avatar
+        });
+    })
+    res.send(tweetsToReturn);
 });
 
 const database = {
-    users: [],
+    users: [
+    {
+        username: "bobesponja",
+        avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        username: "patrick",
+        avatar: "https://i.pinimg.com/736x/37/aa/f2/37aaf27bee442dfe2fb88455775cf862--patrick-obrian-spongebob-squarepants.jpg"
+    },
+    {
+        username: "molusco",
+        avatar: "https://pbs.twimg.com/profile_images/409413126/lulamolusco2_400x400.jpg"
+    }],
     tweets: [
         {
             username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub"
-        },
+            tweet: "Eu gosto de encher o saco das pessoas que eu amo"
+        }
+        ,
         {
             username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 2"
-        },
+            tweet: "Eu amo o hub"
+        }
+        ,
         {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 3"
-        },
+            username: "patrick",
+            tweet: "Sei lá, acho que minha vida tá coisada"
+        }
+        ,
         {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 4"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 5"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 6"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 7"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 8"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 9"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 10"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 11"
-        },
-        {
-            username: "bobesponja",
-            avatar: "https://yt3.ggpht.com/ytc/AKedOLQ6Ief26j8b1lgSA1OpXSCzJBlnlEEsWtQAfdwB=s900-c-k-c0x00ffffff-no-rj",
-            tweet: "eu amo o hub 12"
-        },
+            username: "molusco",
+            tweet: "Acredite na fantasia que vc quiser mas faça isso longe de mim"
+        }
     ]
+}
+
+function isValidImage(url)
+{
+    if (typeof url !== 'string') {
+        return false;
+      }
+      return (url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) !== null);
 }
 
 app.listen('5000', () => {
